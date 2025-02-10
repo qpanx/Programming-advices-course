ee120ff1a4a7688ba8a7a6079bad393e69ab15b0
Ahmed Wael Maarouf <98268557+qpanx@users.noreply.github.com>
2025-02-10 05:43:50 +0300

    Add files via upload

---
+++ Course3/problem#39/problem#39/problem#39.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a;
+    float b;
+    cout << "Totalbill substric cashpaid\n";
+    cin >> a >> b;
+
+    float total = b - a;
+    cout << "The result is: " << total << endl;
+
+}

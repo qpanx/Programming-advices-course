6a461283ddaad8c24b095c835f2511aad3b34cbd
Ahmed Wael Maarouf <98268557+qpanx@users.noreply.github.com>
2025-02-10 05:25:50 +0300

    Add files via upload

---
+++ Course3/Homework3/Homework3/Homework3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int A;
+	int B;
+
+	cout << "pleas inter number for var A" << endl;
+	cin >> A;
+
+	cout << "Inter number for var B" << "\n";
+	cin >> B;
+
+	cout << A <<"+" << B << "= "  << A + B << endl;
+	cout << A << " - " << B << " = " << A - B << endl;
+	cout << A << " * " << B << " = " << A * B << endl;
+	cout << A << " / " << B << " = " << A / B << endl;
+	cout << A << " % " << B << " = " << A % B << endl;
+}

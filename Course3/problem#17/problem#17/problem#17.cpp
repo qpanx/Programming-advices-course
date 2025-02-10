6e3fc4957efbdbb820c1c4d664a5c7b9434849a9
Ahmed Wael Maarouf <98268557+qpanx@users.noreply.github.com>
2025-02-10 05:38:20 +0300

    Add files via upload

---
+++ Course3/problem#17/problem#17/problem#17.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    unsigned int a;
+    unsigned int b;
+
+    cout << "This program calculate triangle area\n";
+    cin >> a >> b;
+    unsigned sum =0.5* a * b;
+    cout << "The triangle area is " << sum << endl;
+}

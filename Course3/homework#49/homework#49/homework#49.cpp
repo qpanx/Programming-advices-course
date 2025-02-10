47bff1e6bb92fba5518b1891854a92b28df06c89
Ahmed Wael Maarouf <98268557+qpanx@users.noreply.github.com>
2025-02-10 06:54:25 +0300

    Add files via upload

---
+++ Course3/homework#49/homework#49/homework#49.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    //for (int i = 65; i <='Z'; i++) {
+    //    
+    //    for (int j = 65; j <= 'Z'; j++) {
+    //        cout << (char)i << char(j) << endl;
+    //    }
+    //    cout << endl;
+    //}
+
+
+    /*for (int i = 1; i <= 10; i++) {
+        for (int j = 10; j >= i; j--) {
+            cout << "*";
+        }
+        cout << endl;
+    }*/
+
+    /*for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= 11 - i; j++) {
+            cout <<  j;
+        }
+        cout << endl;
+    }*/
+
+
+    /*for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << j;
+        }
+        cout << endl;
+    }
+    */
+
+    /*for (int i = 1; i <= 6; i++) {
+        for (int j = 65; j < 65 + i; j++) {
+            cout << (char)j;
+        }
+        cout << endl;
+    }*/
+
+    //
+    //for (int i = 1; i <= 10; i++) {
+    //    for (int j = 1; j <= 11- i; j++) {
+    //        cout << j;
+    //    }
+    //    cout << endl;
+    //}
+
+   /* for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= 11 - i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }*/
+
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << j;
+        }
+        cout << " " << endl;
+    }
+
+}
+

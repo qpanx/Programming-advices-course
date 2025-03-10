288e73c9b9294577a2cbb1485e5343c58d1003ae
Ahmed Wael Maarouf <98268557+qpanx@users.noreply.github.com>
2025-03-10 08:29:25 +0200

    Add files via upload

---
+++ Course4/problem#1/problem#1/problem#1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+void PrintName(string name) {
+    cout << "\n Your Name is : "  << name << endl;
+}
+
+int main()
+{
+    PrintName("Ahmed");
+}

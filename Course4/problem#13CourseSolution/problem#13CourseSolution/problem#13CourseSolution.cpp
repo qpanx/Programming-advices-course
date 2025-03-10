e87ec77d0bc2030d92489c6b75b5b14e42764968
Ahmed Wael Maarouf <98268557+qpanx@users.noreply.github.com>
2025-03-10 09:17:36 +0200

    Add files via upload

---
+++ Course4/problem#13CourseSolution/problem#13CourseSolution/problem#13CourseSolution.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void ReadUser(int& Num1, int& Num2, int& Num3) {
+    cout << "Enter Three Numbers i will see which one is big.\n";
+
+    cout << "[1]";
+    cin >> Num1;
+
+    cout << "[2]";
+    cin >> Num2;
+
+    cout << "[3]";
+    cin >> Num3;
+}
+
+int CheckMax(int Num1, int Num2, int Num3) {
+    if (Num1 > Num2)
+        if (Num1 > Num3)
+            return Num1;
+        else
+            return Num3;
+    else
+        if (Num2 > Num3)
+            return Num2;
+        else
+            return Num3;
+}
+
+void PrintResult(int Max) {
+    cout << "The large Number is => " << Max;
+}
+
+int main()
+{
+    int Num1, Num2, Num3;
+    ReadUser(Num1, Num2, Num3);
+    PrintResult(CheckMax(Num1, Num2, Num3));
+}

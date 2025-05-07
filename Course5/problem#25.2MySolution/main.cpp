d35beb161fdf35b3296962ef2d9157b820ae3ac2
Ahmed Wael Maarouf <98268557+qpanx@users.noreply.github.com>
2025-05-07 21:06:22 +0300

    Add files via upload

---
+++ Course5/problem#25.2MySolution/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace  std;
+
+
+
+int RandomNumber(int From , int To) {
+    int randnum = rand() % (To - From + 1) + From;
+    return randnum;
+}
+void FillArrayWithRandomNumbers(int arr[100], int &arrLength) {
+    cout << "\nEnter number of elements:\n";
+    cin >> arrLength;
+
+    for (int i = 0; i < arrLength; i++) {
+        arr[i] = RandomNumber(1, 100);
+    }
+}
+
+void PrintArray(int arr[100], int arrLength) {
+    cout << "Array Elements: ";
+    for (int i = 0; i < arrLength; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int MinNumberInArray(int arr[100], int arrLength) {
+    int Min = 0;
+    Min = arr[0];
+    for (int i = 0; i < arrLength; i++) {
+        if (arr[i] < Min) {
+            Min = arr[i];
+        }
+
+    }
+    return Min;
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+
+    int arr[100];
+    int arrLength ;
+
+    FillArrayWithRandomNumbers(arr, arrLength);
+
+    cout << "\nArray Elements: ";
+    PrintArray(arr, arrLength);
+
+    cout << "\nMin Number: " << MinNumberInArray(arr, arrLength);
+
+    return 0;
+}

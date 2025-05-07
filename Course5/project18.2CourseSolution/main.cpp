8753d2ddbd03c77b330704f85ff34d1fb9e5f186
Ahmed Wael Maarouf <98268557+qpanx@users.noreply.github.com>
2025-05-07 20:52:56 +0300

    Add files via upload

---
+++ Course5/project18.2CourseSolution/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ReadText() {
+    string Text ;
+
+    cout << "Please enter Text: "<< endl;
+    getline(cin, Text);
+
+    return Text;
+}
+
+string EncryptText(string Text , short EncryptionKey) {
+
+    for (int i = 0; i <= Text.length(); i++) {
+        Text[i] = char((int) Text[i] + EncryptionKey);
+    }
+    return Text;
+}
+
+string DecryptText(string Text , short EncryptionKey) {
+
+    for (int i = 0; i <= Text.length(); i++) {
+        Text[i] = char((int) Text[i] - EncryptionKey);
+    }
+    return Text;
+}
+
+int main()
+{
+    const short EncryptionKey = 2;
+
+    string Text = ReadText();
+    string TextAfterEncryption = EncryptText(Text, EncryptionKey);
+    string TextAfterDecryption = DecryptText(TextAfterEncryption, EncryptionKey);
+
+    cout << "\nText Before Encryption: " << Text << endl;
+    cout << "Text After Encryption: " << TextAfterEncryption << endl;
+    cout << "Text After Decryption: " << TextAfterDecryption << endl;
+
+    return 0;
+}

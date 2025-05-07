487b8bf5f9d8b2abcd43e05da1c98f29f6174b81
Ahmed Wael Maarouf <98268557+qpanx@users.noreply.github.com>
2025-05-07 21:26:45 +0300

    Add files via upload

---
+++ Course5/problem#43_2MySolution/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace  std;
+
+int RandomNumber(int From , int To) {
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int &arrLength) {
+    cout << "\nEnter number of elements:\n";
+    cin >> arrLength;
+
+    for (int i = 0; i < arrLength; i++) {
+        arr[i] = RandomNumber(1, 100);
+    }
+}
+
+void PrintArray(int arr[100], int arrLength) {
+    for (int i = 0; i < arrLength; i++) {
+        cout << arr[i] << " " ;
+    }
+    cout << endl ;
+}
+
+int EvenCount(int arr[100], int arrLength) {
+    int Counter = 0;
+
+    for (int i = 0; i < arrLength; i++) {
+        if (arr[i] % 2 == 0) {
+            Counter++;
+        }
+    }
+
+    return Counter;
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+
+    int arr[100], arrLength = 0 ,  Counter;
+
+    FillArrayWithRandomNumbers(arr, arrLength);
+
+    cout << "\nArray Elements: ";
+    PrintArray(arr, arrLength);
+
+    cout << "\nEven Numbers: ";
+    cout << EvenCount(arr, arrLength)<< endl;
+
+    return 0;
+}

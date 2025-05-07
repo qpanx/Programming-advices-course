a3a7c54baab8c5b0f519ca2a4debbc20f27765c8
Ahmed Wael Maarouf <98268557+qpanx@users.noreply.github.com>
2025-05-07 20:49:45 +0300

    Add files via upload

---
+++ Course5/problem#16.2CourseSolution/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+void PrintWordsFromAAAtoZZZ() {
+    cout << "\n";
+    string word = "";
+    for (int i = 65; i <= 90; i++) {
+        for (int j = 65; j <= 90; j++) {
+            for (int k = 65; k <= 90; k++) {
+                word = word + char(i);
+                word = word + char(j);
+                word = word + char(k);
+
+                cout << word << endl;
+
+                word = "";
+            }
+        }
+
+        cout << "\n________________________\n";
+    }
+}
+
+
+int main()
+{
+    PrintWordsFromAAAtoZZZ();
+
+    return 0;
+}
